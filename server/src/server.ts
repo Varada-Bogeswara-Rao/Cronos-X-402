ddaf8a469a9b89f9cc12184013225b2ca7ed052a
Varada-Bogeswar-Rao <322103310245@gvpce.ac.in>
2025-12-21 15:49:11 +0530

    trust proxy

--- server/src/server.ts
+++ server/src/server.ts
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: '10kb' }));
-
+app.set("trust proxy", 1);
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100
